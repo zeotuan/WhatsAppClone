6ce631333dccc7c7db0fcfa44f9c0388ac4c38bf
Tuan Pham <zeotuan@gmail.com>
2021-07-05 13:04:07 +1000

    refractor contact screen to load user from database

--- screens/ContactScreen.tsx
+++ screens/ContactScreen.tsx
@@ -2,17 +2,38 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
 import ContactListItem from '../components/ContactListItem';
-import UserDummy from '../data/Users';
+//import UserDummy from '../data/Users';
 import {FlatList} from 'react-native'
 import NewMessageButton from '../components/NewMessageButton';
+import { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listUsers } from '../src/graphql/queries';
 
 export default function ContactScreen() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try{
+        const userData = await API.graphql(
+          graphqlOperation(
+            listUsers 
+          )
+        )
+        // @ts-ignore
+        setUsers(userData.data.listUsers.items);
+      }catch(e){
+        console.log(e);
+      }
+    }
+  },[])
+
   return (
     <View style={styles.container}>
       <FlatList
         style={{width:'100%'}}
-        data={UserDummy}
+        data={users}
         renderItem={({item})=> <ContactListItem user={item}  />}
+        // @ts-ignore
         keyExtractor={(item) => item.id}
       />
       <NewMessageButton />
